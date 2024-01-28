4ec30d514904e400340611d3156791435c81fec7
shamnathasni <shamnathasni4@gmail.com>
2024-01-28 20:44:18 +0530

    prettier implemnted

--- src/Pages/UserPages/UserBookingPage.jsx
+++ src/Pages/UserPages/UserBookingPage.jsx
@@ -24,7 +24,7 @@ function UserBookingPage() {
 
     try {
       const response = await fetch(
-        "http://localhost:3000/api/create-checkout-session",
+        "https://trendtitan.snapshot.shop/api/create-checkout-session",
         {
           method: "post",
           headers: {
