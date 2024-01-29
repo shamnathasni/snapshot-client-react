0a93c9bc677c41d4f9f6b9872cff3b93c08c7569
shamnathasni <shamnathasni4@gmail.com>
2024-01-29 13:12:39 +0530

    code modified

--- src/Pages/AdminPages/AdminUserList.jsx
+++ src/Pages/AdminPages/AdminUserList.jsx
@@ -112,7 +112,7 @@ function AdminUserList() {
                 ) : (
                   <div className="flex items-center justify-center h-full">
                     <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-[#872341]"></div>
-                    <div>No user found</div>
+                    
                   </div>
                 )}
               </div>
